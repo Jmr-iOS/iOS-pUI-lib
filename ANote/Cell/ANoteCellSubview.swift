8fdf36f65bd02d9af425a381c240ed66823852ea
justinmreina <justinmreina@gmail.com>
2018-01-22 12:15:43 -0800

    (+)    add title field and correctly dismissing keyboards to       CellSubview

--- ANote/Cell/ANoteCellSubview.swift
+++ ANote/Cell/ANoteCellSubview.swift
@@ -10,8 +10,8 @@
  *      add main text           (UITextView)
  *      add bottom menubar      (UIView)
  *      ...
- *      keyboard dismisses
  *      main view here is scrollable
+ *      mainText & titleText feature 'return' keyboard (after menu bar added for keyboard dismissal)
  *
  *  @section    Data Architecture
  *      each row represents a data entry whose data is captured in completion by the row's cell subview, presented here. all data
@@ -25,25 +25,26 @@
 import UIKit
 
 
-class ANoteCellSubview : UIView {
+class ANoteCellSubview : UIView, UITextFieldDelegate, UITextViewDelegate {
     
     //Ref
     var mainView     : UIView!;                                     /* main view of app                                             */
     var parentCell   : ANoteTableViewCell!;
     
     //UI
-    var topBar    : UIView;
-    var titleBar  : UIView;
-    var dateBar   : UIView;
-    var datePlace : UIView;
-    var mainText  : UITextView;
-    var menuBar   : UIView;
+    var topBar     : UIView;
+    var titleBar   : UIView;
+    var titleField : UITextField;
+    var dateBar    : UIView;
+    var datePlace  : UIView;
+    var mainText   : UITextView;
+    var menuBar    : UIView;
     
-    //PrevUI
+    //Temp-UI
     var retButton  : UIButton!;                                     /* return button of the subview                                 */
     
     //Config
-    private let verbose : Bool = false;                             /* for this class                                               */
+    private let verbose : Bool = true;                              /* for this class                                               */
     
     private static var bkgnd_ctr : Int =  0;                        /* background index to use for specified call                   */
     var bkgnd_ind : Int;                                            /* index of background for cell                                 */
@@ -52,7 +53,7 @@ class ANoteCellSubview : UIView {
     var temp_i : Int = 0;
     var rslts = [String]();
     
-    
+
     /********************************************************************************************************************************/
 	/**	@fcn		init(mainView : UIView, parentCell : ANoteTableViewCell)
 	 *  @brief		x
@@ -72,6 +73,7 @@ class ANoteCellSubview : UIView {
         //Init UI
         topBar = UIView();
         titleBar = UIView();
+        titleField = UITextField();
         dateBar = UIView();
         datePlace = UIView();
         mainText = UITextView();
@@ -102,6 +104,7 @@ class ANoteCellSubview : UIView {
         //Track view offset
         var y : CGFloat = UIApplication.shared.statusBarFrame.height;       /* height to begin view placement                       */
         
+        
         //**************************************************************************************************************************//
         //                                              TOP BAR                                                                     //
         //**************************************************************************************************************************//
@@ -109,13 +112,28 @@ class ANoteCellSubview : UIView {
         topBar.backgroundColor = UIColor.lightGray;
         y = (y + topBar.bounds.height);
         
+        
         //**************************************************************************************************************************//
         //                                              TITLE BAR                                                                   //
         //**************************************************************************************************************************//
         titleBar.frame = CGRect(x: 0, y: y, width: UIScreen.main.bounds.width, height: 40);
-        titleBar.backgroundColor = UIColor.red;
         y = (y + titleBar.bounds.height);
 
+        //Gen text field
+        titleField.frame = CGRect(x: 13, y: -1, width: titleBar.frame.width-13, height: titleBar.frame.height);
+        titleField.placeholder = "Untitled";
+        titleField.font = UIFont(name: "HelveticaNeue-Medium", size: 46);
+        titleField.font = FontUtils.updateFontSize(titleField.font!, 26);
+        titleField.clearButtonMode = .always;
+        titleField.clearsOnBeginEditing = true;
+        titleField.textColor = UIColor.darkGray;
+        titleField.returnKeyType = UIReturnKeyType.done;
+        titleField.delegate = self;
+        
+        //Add to bar
+        titleBar.addSubview(titleField);
+        
+        
         //**************************************************************************************************************************//
         //                                               DATE BAR                                                                   //
         //**************************************************************************************************************************//
@@ -123,6 +141,7 @@ class ANoteCellSubview : UIView {
         dateBar.backgroundColor = UIColor.darkGray;
         y = (y + dateBar.bounds.height);
 
+        
         //**************************************************************************************************************************//
         //                                             DATE VIEW                                                                    //
         // @todo    only inserted when date is present                                                                              //
@@ -131,13 +150,17 @@ class ANoteCellSubview : UIView {
         datePlace.backgroundColor = UIColor.blue;
         y = (y + datePlace.bounds.height);
         
+        
         //**************************************************************************************************************************//
         //                                             MAIN TEXT                                                                    //
         //**************************************************************************************************************************//
         mainText.frame = CGRect(x: 0, y: y, width: UIScreen.main.bounds.width, height: 312);
-        mainText.backgroundColor = UIColor.orange;
-        mainText.text = "main";
+        mainText.returnKeyType = UIReturnKeyType.done;
+        mainText.backgroundColor = nil;
+        mainText.delegate = self;
+        mainText.text = "";
         y = (y + mainText.bounds.height);
+        
         
         //**************************************************************************************************************************//
         //                                             MENU BAR                                                                     //
@@ -264,11 +287,11 @@ class ANoteCellSubview : UIView {
         
         //Slide in View
         UIView.animate(withDuration: launch_dur_s, delay: launch_del_s, options: UIViewAnimationOptions.transitionCrossDissolve, animations: {
-            print("CellSubview.dismissSubView():       sliding view out");
+            if(self.verbose) { print("CellSubview.dismissSubView():       sliding view out"); }
             self.alpha = 1.0;
             self.frame = getCSFrame(onscreen: false);
         }, completion: { (finished: Bool) -> Void in
-            print("CellSubview.dismissSubView():       sliding view out completion");
+            if(self.verbose) { print("CellSubview.dismissSubView():       sliding view out completion"); }
             self.setContentsAlpha(0);
             self.frame = getCSFrame(onscreen: false);
         });
@@ -277,8 +300,60 @@ class ANoteCellSubview : UIView {
         
         return;
     }
+
+
+//**********************************************************************************************************************************//
+//                                                     UITEXTFIELD DELEGATE                                                         //
+//**********************************************************************************************************************************//
+
+    /********************************************************************************************************************************/
+    /* @fcn       textFieldShouldReturn(_ textField : UITextField) -> Bool                                                          */
+    /* @details   dismiss keyboard on completion                                                                                    */
+    /********************************************************************************************************************************/
+    func textFieldShouldReturn(_ textField : UITextField) -> Bool {
+        
+        //dismiss
+        textField.resignFirstResponder();
+        
+        if(verbose) { print("ViewController.txtFieldShldRtrn():  return key pressed and exiting"); }
+        
+        return true;                                                        /* normal behavior                                      */
+    }
+    
+
+//**********************************************************************************************************************************//
+//                                                     UITEXTVIEW DELEGATE                                                          //
+//**********************************************************************************************************************************//
+    
+    /********************************************************************************************************************************/
+    /* @fcn       textViewShouldEndEditing(_ textView: UITextView) -> Bool                                                          */
+    /* @details   dismiss keyboard on completion       (1/2)                                                                        */
+    /********************************************************************************************************************************/
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        if(verbose) { print("ViewController.txtViewShldEnd():    return key pressed and signaling exit"); }
+        return true;
+    }
     
     
+    /********************************************************************************************************************************/
+    /* @fcn       textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool         */
+    /* @details   dismiss keyboard on completion (2/2)                                                                              */
+    /********************************************************************************************************************************/
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            mainText.resignFirstResponder();
+            if(verbose) { print("ViewController.textView(SCT):       return key pressed and exiting"); }
+            return false;
+        }
+
+        return true;
+    }
+    
+    
+    /********************************************************************************************************************************/
+    /* @fcn       required init?(coder aDecoder: NSCoder)                                                                           */
+    /* @details   x                                                                                                                 */
+    /********************************************************************************************************************************/
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented"); }
 }
 
